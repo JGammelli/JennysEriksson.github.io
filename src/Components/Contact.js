1bcc04ce441d7e348567ee014f57d8f98620efce
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2021-09-13 12:44:08 +0200

    updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,17 +4,20 @@ class Contact extends React.Component{
     render(){
         return(
             <section id="contactSection">
-                <div className="contactBox">
-                    <p>Get in touch!</p>
-                    <h1>Contact</h1>
-                </div>
-                <div className="contactInfo">
-                <p>Email: eriksson.jenny97@gmail.com</p>
-                </div>
-                <div className="siteByInfo">
-                    <h4>Site by</h4>
-                    <p>Judith Gammelli</p>
-                </div>
+                    <div className="contactWrap">
+                        <div className="contactBox">
+                            <p>Get in touch!</p>
+                            <h1>Contact:</h1>
+                        </div>
+                        <div className="contactInfo">
+                            <h3>Jenny Eriksson</h3>
+                            <p>Email: eriksson.jenny97@gmail.com</p>
+                        </div>
+                        <div className="siteByInfo">
+                            <h4>Site by</h4>
+                            <p>Judith Gammelli</p>
+                        </div>
+                    </div>
             </section>
         );
     }
